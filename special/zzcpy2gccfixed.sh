ccc057bb786a20319f4f6cb2f51c4646108e0de6
David Lowes <davidl.david@gmail.com>
2020-05-05 06:49:38 +0300

    Rely on header files and gnu m4 from IBM yum repository

--- special/zzcpy2gccfixed.sh
+++
@@ -1,7 +0,0 @@
-#!/QOpenSys/usr/bin/ksh
-cp include/*.h /QOpenSys/opt/freeware/lib/gcc/powerpc-ibm-aix6.1.0.0/4.8.3/include-fixed/.
-cp cmake/*400* /opt/freeware/share/cmake/Modules/Platform/.
-cp bin/m4 /opt/freeware/bin/.
-cp bin/build-zenddbi.sh /usr/src/zenddbi/.
-cp bin/build-tony.sh /usr/src/zenddbi/.
-
